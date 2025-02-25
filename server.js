71a82b590795c581df7f29bcfaec82ff9ad68aba
RohanVellanki <vellankirohan2006@gmail.com>
2025-02-25 17:50:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,53 +1,35 @@
+// server.js
 const express = require('express');
 const http = require('http');
-const { Server } = require('socket.io');
-const cors = require('cors');
+const socketIo = require('socket.io');
 
 const app = express();
-app.use(cors());
-
 const server = http.createServer(app);
-const io = new Server(server, {
+const io = socketIo(server, {
   cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"]
+    origin: '*',
   }
 });
 
-const cursors = {};
-
 io.on('connection', (socket) => {
-  console.log('User connected');
-
-  socket.on('cursor-move', (data) => {
-    cursors[socket.id] = {
-      x: data.x,
-      y: data.y,
-      name: data.name || 'Anonymous'
-    };
-    socket.broadcast.emit('cursors-update', cursors);
-  });
+  console.log('New client connected');
 
   socket.on('drawing-start', (data) => {
-    socket.broadcast.emit('drawing-start', { ...data, id: socket.id });
+    socket.broadcast.emit('drawing-start', data);
   });
 
   socket.on('drawing', (data) => {
-    socket.broadcast.emit('drawing', { ...data, id: socket.id });
+    socket.broadcast.emit('drawing', data);
   });
 
   socket.on('drawing-end', () => {
-    socket.broadcast.emit('drawing-end', { id: socket.id });
+    socket.broadcast.emit('drawing-end');
   });
 
   socket.on('disconnect', () => {
-    delete cursors[socket.id];
-    io.emit('cursors-update', cursors);
-    console.log('User disconnected');
+    console.log('Client disconnected');
   });
 });
 
 const PORT = process.env.PORT || 3001;
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
